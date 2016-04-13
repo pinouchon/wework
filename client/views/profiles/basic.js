03a6321dfae0619408be73debc07b4426e1ec177
Benjamin Crouzier <pinouchon@gmail.com>
2016-04-13 11:42:01 +0200

    useraccounts

--- client/views/profiles/basic.js
+++ client/views/profiles/basic.js
@@ -1,57 +1,4 @@
-Orders = new Meteor.Collection('orders', {connection: null});
 
-Schemas.contactInformation = new SimpleSchema({
-  name:{
-    type: String,
-    label: 'Name'
-  },
-  address: {
-    type: String,
-    label: 'Address'
-  },
-  zipcode: {
-    type: String,
-    label: 'Zipcode'
-  },
-  city: {
-    type: String,
-    label: 'City'
-  }
-});
-
-Schemas.paymentInformation = new SimpleSchema({
-  paymentMethod: {
-    type: String,
-    label: 'Payment method',
-    allowedValues: ['credit-card', 'bank-transfer'],
-    autoform: {
-      options: [{
-        label: 'Credit card',
-        value: 'credit-card'
-      }, {
-        label: 'Bank transfer',
-        value: 'bank-transfer'
-      }]
-    }
-  },
-  acceptTerms: {
-    type: Boolean,
-    label: 'I accept the terms and conditions.',
-    autoform: {
-      label: false
-    },
-    autoValue: function() {
-      if (this.isSet && this.value !== true) {
-        this.unset();
-      }
-    }
-  }
-});
-
-Orders.attachSchema([
-  Schemas.contactInformation,
-  Schemas.paymentInformation
-]);
 
 Template.basic.helpers({
   steps: function() {
@@ -76,29 +23,5 @@ Template.basic.helpers({
         });
       }
     }];
-  }
-});
-
-Wizard.useRouter('iron:router');
-
-Router.route('/basic/:step?', {
-  name: 'basic',
-  onBeforeAction: function() {
-    console.log(this.params.step);
-    if (!this.params.step) {
-      this.redirect('basic', {
-        step: 'contact-information'
-      });
-    } else {
-      this.next();
-    }
-  }
-});
-
-Router.route('/orders/:_id', {
-  name: 'viewOrder',
-  template: 'viewOrder',
-  data: function() {
-    return Orders.findOne(this.params._id);
   }
 });
