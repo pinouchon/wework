6eeebed047ac4ff75b9b5259bef6584a065d3a91
Benjamin Crouzier <pinouchon@gmail.com>
2016-04-13 11:29:32 +0200

    custom inputs

--- server/lib/environment.js
+++ server/lib/environment.js
@@ -1,12 +1,3 @@
-
-Meteor.startup(function() {
-	//App.accessRule('https://meteor.local');
-       
-	console.log('Configuring content-security-policy:');
-	//BrowserPolicy.content.allowSameOriginForAll();
-	//BrowserPolicy.content.allowOriginForAll('http://meteor.local');
-    });
-
 if (Meteor.settings && Meteor.settings.stripe && Meteor.settings.stripe.secretKey) {
     Stripe = StripeSync(Meteor.settings.stripe.secretKey);
 }else{
